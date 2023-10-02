3624b39053db1a39e8c10ec6c252a6a4202c2b28
MaltaWebDev <MaltaWebDev@gmail.com>
2023-10-02 19:49:14 +0200

    Add Skill component to SkillList

--- src/Skillist.jsx
+++ src/Skillist.jsx
@@ -1,5 +1,29 @@
+import PropTypes from "prop-types";
+
 function SkillList() {
-  return <></>;
+  return (
+    <div className="skill-list">
+      <Skill lang="React" emoji="💪" color="orange" />
+      <Skill lang="Javascript" emoji="💪" color="yellow" />
+      <Skill lang="HTML + CSS" emoji="💪" color="blue" />
+      <Skill lang="Svelte" emoji="👶" color="red" />
+    </div>
+  );
 }
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ backgroundColor: props.color }}>
+      <span>{props.lang}</span>
+      <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+Skill.propTypes = {
+  lang: PropTypes.string.isRequired,
+  emoji: PropTypes.string.isRequired,
+  color: PropTypes.string,
+};
 
 export default SkillList;
