cf3fae1386ae901626b88298c3e9c02968419150
MaltaWebDev <MaltaWebDev@gmail.com>
2023-11-28 17:55:45 +0100

    Add additional interests to intro

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -1,6 +1,6 @@
 const data = {
   intro:
-    "Hello my name is Gus, I'm a developer based in Malta, EU. My specialities are React.js with Next.js and Tailwind. I also love programming in Python.",
+    "Hello my name is Gus, I'm a developer based in Malta, EU. My specialities are React.js with Next.js and Tailwind. I also love programming in Python, travelling, and training jiu-jitsu.",
   name: "Angus Girvan",
 };
 
