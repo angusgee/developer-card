06223c871cf617ef549a9bc6b7470ac011f5bc7e
MaltaWebDev <MaltaWebDev@gmail.com>
2023-11-28 08:45:08 +0100

    Refactor SkillList component to use dynamic data

--- src/Skillist.jsx
+++ src/Skillist.jsx
@@ -1,28 +1,43 @@
 import PropTypes from "prop-types";
 
+const skills = [
+  {
+    skill: "HTML + CSS",
+    level: "Advanced",
+    color: "#3498db",
+  },
+  { skill: "Javascript", level: "Advanced", color: "#f1c40f" },
+  { skill: "React", level: "Advanced", color: "#e67e22" },
+  { skill: "Svelte", level: "Beginner", color: "#e74c3c" },
+];
+
 function SkillList() {
-  return (
-    <div className="skill-list">
-      <Skill lang="React" emoji="💪" color="orange" />
-      <Skill lang="Javascript" emoji="💪" color="yellow" />
-      <Skill lang="HTML + CSS" emoji="💪" color="blue" />
-      <Skill lang="Svelte" emoji="👶" color="red" />
-    </div>
-  );
+  const skillList = skills.map((skill, index) => {
+    return (
+      <div className="skill-list" key={index}>
+        <Skill
+          skill={skill.skill}
+          level={skill.level === "Advanced" ? "💪" : "👶"}
+          color={skill.color}
+        />
+      </div>
+    );
+  });
+  return <div>{skillList}</div>;
 }
 
 function Skill(props) {
   return (
     <div className="skill" style={{ backgroundColor: props.color }}>
-      <span>{props.lang}</span>
-      <span>{props.emoji}</span>
+      <span>{props.skill}</span>
+      <span>{props.level}</span>
     </div>
   );
 }
 
 Skill.propTypes = {
-  lang: PropTypes.string.isRequired,
-  emoji: PropTypes.string.isRequired,
+  skill: PropTypes.string.isRequired,
+  level: PropTypes.string.isRequired,
   color: PropTypes.string,
 };
 
